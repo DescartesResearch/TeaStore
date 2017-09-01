8b5fa74f4d0251301ed0cec11f01c005b7f14694
Norbert Schmitt <norbert.schmitt@uni-wuerzburg.de>
2017-09-01 09:24:36 +0200

    Builds working but config is not

---
+++ utilities/tools.descartes.petsupplystore.dockerbase/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Replace variables
+sed -i "s/%%SERVICE_PORT%%/${SERVICE_PORT}/g" /usr/local/tomcat/conf/context.xml
+sed -i "s/%%HOST_NAME%%/${HOST_NAME}/g" /usr/local/tomcat/conf/context.xml
+sed -i "s/%%REGISTRY%%/${REGISTRY}/g" /usr/local/tomcat/conf/context.xml
+sed -i "s/%%DB_HOST%%/${DB_HOST}/g" /usr/local/tomcat/conf/context.xml
+sed -i "s/%%DB_PORT%%/${DB_PORT}/g" /usr/local/tomcat/conf/context.xml
+
+# Run tomcat
+exec "$@"
