6b013b411cad9dc74c22412bcc16d3872265039e
Jóakim von Kistowski <joakim.kistowski@uni-wuerzburg.de>
2017-10-12 16:41:22 +0200

    memory config script

--- utilities/tools.descartes.petsupplystore.dockerbase/start.sh
+++ utilities/tools.descartes.petsupplystore.dockerbase/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # DANGER! MAKE SURE THIS FILE HAS UNIX-STYLE LINE-ENDINGS OR THE DOCKER CONTAINER WILL NOT START!
 sed -i "s/%%SERVICE_PORT%%/${SERVICE_PORT}/g" /usr/local/tomcat/conf/context.xml
-sed -i "s/%%HOST_NAME%%/${HOST_NAME}/g" /usr/local/tomcat/conf/context.xml
+if [ "$HOST_NAME" != "unset" ]
+then
+	sed -i "s/%%HOST_NAME%%/${HOST_NAME}/g" /usr/local/tomcat/conf/context.xml
+else
+	sed "/Environment name=\"hostName\"/d" /usr/local/tomcat/conf/context.xml
+fi
 sed -i "s/%%REGISTRY_HOST%%/${REGISTRY_HOST}/g" /usr/local/tomcat/conf/context.xml
 sed -i "s/%%REGISTRY_PORT%%/${REGISTRY_PORT}/g" /usr/local/tomcat/conf/context.xml
 sed -i "s/%%DB_HOST%%/${DB_HOST}/g" /usr/local/tomcat/conf/context.xml
