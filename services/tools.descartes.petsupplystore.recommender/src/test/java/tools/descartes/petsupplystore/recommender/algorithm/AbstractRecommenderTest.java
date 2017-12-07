fd4a56e4ec4fbde9ff0c7db1891ca180b68b5cf9
Johannes Grohmann <johannes.grohmann@uni-wuerzburg.de>
2017-12-07 16:14:15 +0100

    added quantities to unit test

--- services/tools.descartes.petsupplystore.recommender/src/test/java/tools/descartes/petsupplystore/recommender/algorithm/AbstractRecommenderTest.java
+++ services/tools.descartes.petsupplystore.recommender/src/test/java/tools/descartes/petsupplystore/recommender/algorithm/AbstractRecommenderTest.java
@@ -49,7 +49,7 @@ public abstract class AbstractRecommenderTest {
 	 * 
 	 * trainOrders = [{10, user100}, {11, user101}, {12, user103}, {13, user104},
 	 * {14, user101}, {15, user101}, {16, user105}, {17, user106}] orderItems: 10 =
-	 * {1, 2, 3}; 11 = {2, 4}; 12 = {1, 3, 4}; 13 = {2}; 14 = {2}; 15 = {2}; 16 =
+	 * {1, 2, 3}; 11 = {2,2,2,2,2,4,4}; 12 = {1, 3, 4}; 13 = {2,2}; 14 = {2,2}; 15 = {2}; 16 =
 	 * {2, 3, 4, 5}; 17 = {3};
 	 */
 	@Before
@@ -174,13 +174,13 @@ public abstract class AbstractRecommenderTest {
 
 		o = new OrderItem();
 		o.setProductId(4);
-		o.setQuantity(1);
+		o.setQuantity(2);
 		o.setOrderId(11);
 		trainOrderItems.add(o);
 
 		o = new OrderItem();
 		o.setProductId(2);
-		o.setQuantity(1);
+		o.setQuantity(5);
 		o.setOrderId(11);
 		trainOrderItems.add(o);
 
@@ -204,13 +204,13 @@ public abstract class AbstractRecommenderTest {
 
 		o = new OrderItem();
 		o.setProductId(2);
-		o.setQuantity(1);
+		o.setQuantity(2);
 		o.setOrderId(13);
 		trainOrderItems.add(o);
 
 		o = new OrderItem();
 		o.setProductId(2);
-		o.setQuantity(1);
+		o.setQuantity(2);
 		o.setOrderId(14);
 		trainOrderItems.add(o);
 
