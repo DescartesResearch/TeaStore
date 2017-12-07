7c2eceadef21a8c5d02d85a0ec082aede74e3221
Jóakim von Kistowski <joakim.kistowski@uni-wuerzburg.de>
2017-12-07 13:48:54 +0100

    fixed bug in start.sh

--- utilities/tools.descartes.petsupplystore.dockerbase/start.sh
+++ utilities/tools.descartes.petsupplystore.dockerbase/start.sh
@@ -7,7 +7,7 @@ then
 else
 	sed -i "/Environment name=\"hostName\"/d" /usr/local/tomcat/conf/context.xml
 fi
-sed -i "s/<Environment name=\"registryURL\" value=.*/<Environment name=\"registryURL\" value=\"http://${REGISTRY_HOST}:${REGISTRY_PORT}/tools.descartes.petsupplystore.registry/rest/services/\"/g" /usr/local/tomcat/conf/context.xml
+sed -i "s|<Environment name=\"registryURL\" value=.*|<Environment name=\"registryURL\" value=\"http://${REGISTRY_HOST}:${REGISTRY_PORT}/tools.descartes.petsupplystore.registry/rest/services/\"|g" /usr/local/tomcat/conf/context.xml
 sed -i "s/<Environment name=\"databaseHost\" value=.*/<Environment name=\"databaseHost\" value=\"${DB_HOST}\"/g" /usr/local/tomcat/conf/context.xml
 sed -i "s/<Environment name=\"databasePort\" value=.*/<Environment name=\"databasePort\" value=\"${DB_PORT}\"/g" /usr/local/tomcat/conf/context.xml
 if [ "$PROXY_NAME" != "unset" ] && [ "$PROXY_PORT" != "unset" ]
