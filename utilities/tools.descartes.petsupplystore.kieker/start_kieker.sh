37232c998447f71fdc27e6422fcc75981d02b01d
Simon <simon.eismann@uni-wuerzburg.de>
2018-01-30 14:08:06 +0100

    updated start_kieker.sh again

--- utilities/tools.descartes.petsupplystore.kieker/start_kieker.sh
+++ utilities/tools.descartes.petsupplystore.kieker/start_kieker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # DANGER! MAKE SURE THIS FILE HAS UNIX-STYLE LINE-ENDINGS OR THE DOCKER CONTAINER WILL NOT START!
-sed -i "s/kieker.monitoring.writer.amqp.AmqpWriter.uri=amqp:\/\/admin:nimda@RABBITMQ_PORT_PLACEHOLDER/kieker.monitoring.writer.amqp.AmqpWriter.uri=amqp:\/\/admin:nimda@${RABBITMQ_HOST}/g" /kieker/config/kieker.monitoring.properties
+sed -i "s/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@RABBITMQ_PORT_PLACEHOLDER/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@${RABBITMQ_HOST}/g" /kieker/config/kieker.monitoring.properties
