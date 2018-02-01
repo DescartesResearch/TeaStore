5b9b9db52cad13d0c42281b3cfd58984925df639
Simon <simon.eismann@uni-wuerzburg.de>
2018-02-01 10:13:10 +0100

    switched logging to one file

--- utilities/tools.descartes.petsupplystore.kieker.rabbitmq/src/main/java/tools/descartes/petsupplystore/kieker/rabbitmq/FileWriterDaemon.java
+++ utilities/tools.descartes.petsupplystore.kieker.rabbitmq/src/main/java/tools/descartes/petsupplystore/kieker/rabbitmq/FileWriterDaemon.java
@@ -13,22 +13,19 @@ public class FileWriterDaemon implements Runnable {
 
 	@Override
 	public void run() {
-		Logger logger = Logger.getLogger("FileWriterDaemon");
-		logger.setLevel(Level.DEBUG);
-		logger.info("Starting FileWriterDaemon!");
+		new File("logs").mkdir();
+		new File("logs").mkdirs();
+		Configuration configuration = new Configuration();
+
+		configuration.setProperty(AsciiFileWriter.CONFIG_PATH, "logs");
+		configuration.setProperty(AsciiFileWriter.CONFIG_MAXENTRIESINFILE, "-1");
+		configuration.setProperty(AsciiFileWriter.CONFIG_MAXLOGSIZE, "-1");
+		configuration.setProperty(AsciiFileWriter.CONFIG_MAXLOGFILES, "-1");
+		configuration.setProperty(AsciiFileWriter.CONFIG_FLUSH, "true");
+
+		AsciiFileWriter writer = new AsciiFileWriter(configuration);
 		try {
 			while (true) {
-				new File("logs").mkdir();
-				new File("logs").mkdirs();
-				Configuration configuration = new Configuration();
-
-				configuration.setProperty(AsciiFileWriter.CONFIG_PATH, "logs");
-				configuration.setProperty(AsciiFileWriter.CONFIG_MAXENTRIESINFILE, "-1");
-				configuration.setProperty(AsciiFileWriter.CONFIG_MAXLOGSIZE, "-1");
-				configuration.setProperty(AsciiFileWriter.CONFIG_MAXLOGFILES, "-1");
-				configuration.setProperty(AsciiFileWriter.CONFIG_FLUSH, "true");
-
-				AsciiFileWriter writer = new AsciiFileWriter(configuration);
 				for (IMonitoringRecord record : MemoryLogStorage.getRecords())
 					writer.writeMonitoringRecord(record);
 				try {
@@ -37,7 +34,9 @@ public class FileWriterDaemon implements Runnable {
 					e.printStackTrace();
 				}
 			}
-		} catch (Throwable t) { // Catch Throwable rather than Exception (a subclass).
+		} catch (Throwable t) {
+			Logger logger = Logger.getLogger("FileWriterDaemon");
+			logger.setLevel(Level.INFO);
 			logger.error("Error!", t);
 			}
 	}
