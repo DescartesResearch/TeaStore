31f8232c121169cdcb378d0687ec895c82d4e5f2
Simon <simon.eismann@uni-wuerzburg.de>
2018-02-05 16:11:28 +0100

    First draft for passing of SessionIds

---
+++ utilities/tools.descartes.petsupplystore.registryclient/src/main/java/tools/descartes/petsupplystore/registryclient/rest/TrackingFilter.java
@@ -0,0 +1,42 @@
+package tools.descartes.petsupplystore.registryclient.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class TrackingFilter implements Filter {
+
+	Logger logger = Logger.getLogger(TrackingFilter.class);
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			logger.info(httpRequest.getHeader("KiekerTracingInfo"));
+		} else {
+			logger.error("Something went wrong");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
