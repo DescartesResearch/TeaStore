7035f7b9969a860470c4413cb8f6d35dd0d3b2dd
Jóakim von Kistowski <joakim.kistowski@uni-wuerzburg.de>
2018-03-16 14:57:16 +0100

    waiting for database to come online when launching persistence

--- services/tools.descartes.petsupplystore.persistence/src/main/java/tools/descartes/petsupplystore/persistence/daemons/InitialDataGenerationDaemon.java
+++ services/tools.descartes.petsupplystore.persistence/src/main/java/tools/descartes/petsupplystore/persistence/daemons/InitialDataGenerationDaemon.java
@@ -13,6 +13,7 @@
  */
 package tools.descartes.petsupplystore.persistence.daemons;
 
+import javax.persistence.PersistenceException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -32,6 +33,8 @@ import tools.descartes.petsupplystore.registryclient.RegistryClient;
 public class InitialDataGenerationDaemon implements ServletContextListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(InitialDataGenerationDaemon.class);
+	
+	private static final long DATABASE_OFFLINE_WAIT_MS = 2000;
 	
     /**
      * Default constructor. 
@@ -53,6 +56,7 @@ public class InitialDataGenerationDaemon implements ServletContextListener {
      * @param event The servlet context event at initialization.
      */
     public void contextInitialized(ServletContextEvent event)  { 
+    	waitForDatabase();
     	if (DataGenerator.GENERATOR.isDatabaseEmpty()) {
     		LOG.info("Database is empty. Generating new database content");
     		DataGenerator.GENERATOR.generateDatabaseContent(DataGenerator.SMALL_DB_CATEGORIES,
@@ -64,5 +68,23 @@ public class InitialDataGenerationDaemon implements ServletContextListener {
     	LOG.info("Persistence finished initializing database");
     	RegistryClient.getClient().register(event.getServletContext().getContextPath());
     	LOG.info("Persistence started registration daemon");
+    }
+    
+    private void waitForDatabase() {
+    	boolean databaseOffline = true;
+    	while (databaseOffline) {
+    		try {
+        		DataGenerator.GENERATOR.isDatabaseEmpty();
+        		databaseOffline = false;
+        	} catch (PersistenceException e) {
+        		LOG.warn("Exception connecting to database. Is database offline? Wating for " + DATABASE_OFFLINE_WAIT_MS + " ms.");
+        		try {
+					Thread.sleep(DATABASE_OFFLINE_WAIT_MS);
+				} catch (InterruptedException e1) {
+					LOG.error("Exception waiting for database to come online: " + e1.getMessage());
+				}
+        	}
+    	}
+    	
     }
 }
