e0a078acc3e8f980c763b8299fe54522f3e8bfbe
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-01 17:18:20 +0200

    auth creates artificial load

--- services/tools.descartes.teastore.auth/src/main/java/tools/descartes/teastore/auth/rest/AuthUserActionsREST.java
+++ services/tools.descartes.teastore.auth/src/main/java/tools/descartes/teastore/auth/rest/AuthUserActionsREST.java
@@ -140,6 +140,15 @@ public class AuthUserActionsREST {
 			System.out.println("End Login");
 			return Response.status(Response.Status.OK).entity(blob).build();
 		}
+		for (int i = 0; i < 100; i++) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		if (user != null 
 //				&& BCryptProvider.checkPassword(password, user.getPassword())
 				) {
