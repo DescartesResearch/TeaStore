e773d7a8cddf7ee1450be450b9bcc5e59c215aee
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-01 16:38:21 +0200

    logs with stamps

--- utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/loadbalancers/ServiceLoadBalancer.java
+++ utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/loadbalancers/ServiceLoadBalancer.java
@@ -211,6 +211,8 @@ public final class ServiceLoadBalancer {
         	if (loadBalancer == null || loadBalancer.getAllServers().isEmpty()) {
         		LOG.warn("No Server registered for Service: " + targetService.getServiceName());
         	} else {
+        		long tic = System.currentTimeMillis();
+        		System.out.println("Starting actual Call");
         		ServiceLoadBalancerResult<R> slbr = LoadBalancerCommand.<ServiceLoadBalancerResult<R>>builder()
                         .withLoadBalancer(loadBalancer)
                         .withRetryHandler(retryHandler)
@@ -221,6 +223,7 @@ public final class ServiceLoadBalancer {
                 				.getRESTClient(server), operation)
                         		))
                         .onErrorReturn((Throwable e) -> null).toBlocking().first();
+        		System.out.println("Finished in " + (System.currentTimeMillis() - tic));
         		if (slbr == null) {
         			throw new NullPointerException("ServiceLoadBalancerResult was null!");
         		}
