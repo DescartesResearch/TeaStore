cab74f35d5d2f188e05aa4bd027759d437af1ac7
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-05 17:19:33 +0200

    logging service nolonger deletes itself

--- utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/Reset.java
+++ utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/Reset.java
@@ -19,24 +19,39 @@ public class Reset extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		LogReaderStartup.stopFileWriter();
-		
+
 		MemoryLogStorage.clearMemoryStorage();
-		deleteFolder(new File("apache-tomcat-8.5.24/webapps/logs"));
+		deleteFolder(new File("apache-tomcat-8.5.24/webapps/logs"), "kieker");
 
 		LogReaderStartup.startFileWriter();
 	}
-	
-	public  void deleteFolder(File folder) {
-	    File[] files = folder.listFiles();
-	    if(files!=null) { //some JVMs return null for empty dirs
-	        for(File f: files) {
-	            if(f.isDirectory()) {
-	                deleteFolder(f);
-	            } else {
-	                f.delete();
-	            }
-	        }
-	    }
-	    folder.delete();
+
+	public void deleteFolder(File folder, String prefix) {
+		File[] files = folder.listFiles();
+		if (files != null) { // some JVMs return null for empty dirs
+			for (File f : files) {
+				if (f.getPath().contains(prefix)) {
+					if (f.isDirectory()) {
+						deleteFolder(f);
+					} else {
+						f.delete();
+					}
+				}
+			}
+		}
+	}
+
+	public void deleteFolder(File folder) {
+		File[] files = folder.listFiles();
+		if (files != null) { // some JVMs return null for empty dirs
+			for (File f : files) {
+				if (f.isDirectory()) {
+					deleteFolder(f);
+				} else {
+					f.delete();
+				}
+			}
+		}
+		folder.delete();
 	}
 }
