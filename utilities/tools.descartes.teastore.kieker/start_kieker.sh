5d61e34ada3268bbb9880c5b094c111c5cba6e2c
Simon <simon.eismann@uni-wuerzburg.de>
2018-07-02 17:06:00 +0200

    Revert "added LOG_PARAMETERS flag"
    This reverts commit 4789513cc60c0e075ccaff5b1ee26943500cf055.

--- utilities/tools.descartes.teastore.kieker/start_kieker.sh
+++ utilities/tools.descartes.teastore.kieker/start_kieker.sh
@@ -5,11 +5,3 @@ if [ "$LOG_TO_FILE" == "true" ]
 then
 sed -i "s/kieker.monitoring.writer=kieker.monitoring.writer.collector.ChunkingCollector/kieker.monitoring.writer=kieker.monitoring.writer.filesystem.AsciiFileWriter/g" /kieker/config/kieker.monitoring.properties
 fi
-if [ "$LOG_PARAMETERS" == "true" ]
-then
-sed -i "s/kieker.monitoring.probe.aspectj.operationExecution.AbstractOperationExecutionAspect/tools.descartes.teastore.kieker.probes.AbstractOperationExecutionWithParameterAspect/g" /usr/local/tomcat/webapps/tools.descartes.teastore.webui/WEB-INF/classes/aop.xml
-sed -i "s/kieker.monitoring.probe.aspectj.operationExecution.AbstractOperationExecutionAspect/tools.descartes.teastore.kieker.probes.AbstractOperationExecutionWithParameterAspect/g" /usr/local/tomcat/webapps/tools.descartes.teastore.auth/WEB-INF/classes/aop.xml
-sed -i "s/kieker.monitoring.probe.aspectj.operationExecution.AbstractOperationExecutionAspect/tools.descartes.teastore.kieker.probes.AbstractOperationExecutionWithParameterAspect/g" /usr/local/tomcat/webapps/tools.descartes.teastore.recommender/WEB-INF/classes/aop.xml
-sed -i "s/kieker.monitoring.probe.aspectj.operationExecution.AbstractOperationExecutionAspect/tools.descartes.teastore.kieker.probes.AbstractOperationExecutionWithParameterAspect/g" /usr/local/tomcat/webapps/tools.descartes.teastore.image/WEB-INF/classes/aop.xml
-sed -i "s/kieker.monitoring.probe.aspectj.operationExecution.AbstractOperationExecutionAspect/tools.descartes.teastore.kieker.probes.AbstractOperationExecutionWithParameterAspect/g" /usr/local/tomcat/webapps/tools.descartes.teastore.persistence/WEB-INF/classes/aop.xml
-fi
