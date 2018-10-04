62c292941ccbcc85267889e9ffc629b5f8072159
Simon <simon.eismann@uni-wuerzburg.de>
2018-10-04 14:56:20 +0200

    addressed checkstyle violation

--- utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/IndexServlet.java
+++ utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/IndexServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kieker.common.record.IMonitoringRecord;
-
 /**
  * Servlet that shows logs currently stored in memory.
  * 
