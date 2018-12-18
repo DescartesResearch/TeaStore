ea62e166bc38c1ddbf76c155072aac1c5ee56c89
Simon <simon.eismann@uni-wuerzburg.de>
2018-12-18 14:48:13 +0100

    added quotation marks to setenv command

--- utilities/tools.descartes.teastore.dockerbase/start.sh
+++ utilities/tools.descartes.teastore.dockerbase/start.sh
@@ -28,5 +28,5 @@ if [ "$LOG_TO_FILE" == "true" ] || [ "$RABBITMQ_HOST" != "unset" ]
 then
 touch /usr/local/tomcat/bin/setenv.sh
 chmod +x /usr/local/tomcat/bin/setenv.sh
-echo 'export JAVA_OPTS=-javaagent:/kieker/agent/agent.jar -Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties -Daj.weaving.verbose=false -Dorg.aspectj.weaver.loadtime.configuration=aop.xml -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Daj.weaving.loadersToSkip=java.net.URLClassLoader' > /usr/local/tomcat/bin/setenv.sh
+echo 'export JAVA_OPTS="-javaagent:/kieker/agent/agent.jar -Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties -Daj.weaving.verbose=false -Dorg.aspectj.weaver.loadtime.configuration=aop.xml -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Daj.weaving.loadersToSkip=java.net.URLClassLoader"' > /usr/local/tomcat/bin/setenv.sh
 fi
