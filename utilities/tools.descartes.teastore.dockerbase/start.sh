bdcbb8ce1e28c8f9b708c79fe97f1d1bee4ced59
Simon <simon.eismann@uni-wuerzburg.de>
2018-12-18 14:44:08 +0100

    switched to setenv.sh file for java properties

--- utilities/tools.descartes.teastore.dockerbase/start.sh
+++ utilities/tools.descartes.teastore.dockerbase/start.sh
@@ -26,12 +26,7 @@ fi
 
 if [ "$LOG_TO_FILE" == "true" ] || [ "$RABBITMQ_HOST" != "unset" ]
 then
-echo "${JAVA_OPTS}"
-export JAVA_OPTS="${JAVA_OPTS} -javaagent:/kieker/agent/agent.jar"
-export JAVA_OPTS="${JAVA_OPTS} -Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties"
-export JAVA_OPTS="${JAVA_OPTS} -Daj.weaving.verbose=false"
-export JAVA_OPTS="${JAVA_OPTS} -Dorg.aspectj.weaver.loadtime.configuration=aop.xml"
-export JAVA_OPTS="${JAVA_OPTS} -Dkieker.monitoring.skipDefaultAOPConfiguration=true"
-export JAVA_OPTS="${JAVA_OPTS} -Daj.weaving.loadersToSkip=java.net.URLClassLoader"
-echo "${JAVA_OPTS}"
+touch /usr/local/tomcat/bin/setenv.sh
+chmod +x /usr/local/tomcat/bin/setenv.sh
+echo 'export JAVA_OPTS=-javaagent:/kieker/agent/agent.jar -Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties -Daj.weaving.verbose=false -Dorg.aspectj.weaver.loadtime.configuration=aop.xml -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Daj.weaving.loadersToSkip=java.net.URLClassLoader' > /usr/local/tomcat/bin/setenv.sh
 fi
