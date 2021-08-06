beb7fcc30133831fd9f259d0c99ee967f1ca4166
simontrapp <simon.trapp115@gmail.com>
2021-08-06 12:14:17 +0200

    fixed container startup to avoid kieker crash, kieker now working

--- utilities/tools.descartes.teastore.dockerbase/start.sh
+++ utilities/tools.descartes.teastore.dockerbase/start.sh
@@ -32,14 +32,15 @@ sed -i 's/securerandom.source=file:\/dev.*/securerandom.source=file:\/dev\/urand
 
 if [ "$RABBITMQ_HOST" != "unset" ]
 then
-sed -i "s/kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/kieker.monitoring.writer=kieker.monitoring.writer.collector.ChunkingCollector/g" /kieker/config/kieker.monitoring.properties
-
-sed -i "s/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@RABBITMQ_PORT_PLACEHOLDER/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@${RABBITMQ_HOST}/g" /kieker/config/kieker.monitoring.properties
+  sed -i "s/kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/kieker.monitoring.writer=kieker.monitoring.writer.collector.ChunkingCollector/g" /kieker/config/kieker.monitoring.properties
+  sed -i "s/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@RABBITMQ_PORT_PLACEHOLDER/kieker.monitoring.writer.amqp.ChunkingAmqpWriter.uri=amqp:\/\/admin:nimda@${RABBITMQ_HOST}/g" /kieker/config/kieker.monitoring.properties
+  echo 'Kieker Monitoring is active, wait 3 minutes for rabbitmq service to start before starting this service to avoid "connection refused" and therefore kieker crash'
+  sleep 180 # wait 3 minutes for the rabbitmq container to be ready before starting and connecting to it, otherwise results in kieker crash
 fi
 
 if [ "$LOG_TO_FILE" != "true" ] && [ "$RABBITMQ_HOST" == "unset" ]
 then
-sed -i 's/kieker.monitoring.enabled=true/kieker.monitoring.enabled=false/g' /kieker/config/kieker.monitoring.properties
+  sed -i 's/kieker.monitoring.enabled=true/kieker.monitoring.enabled=false/g' /kieker/config/kieker.monitoring.properties
 fi
 
 touch /usr/local/tomcat/bin/setenv.sh
