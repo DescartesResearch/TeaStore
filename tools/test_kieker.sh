01c3e8bd2c7cabf3d2295289fa6293183d4668d0
simontrapp <simon.trapp115@gmail.com>
2021-08-11 18:16:50 +0200

    started implementing new integration tests

---
+++ tools/test_kieker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Checks the teastore-rabbitmq for traces (works only in 'kieker mode'!)
+
+HOST=$1 # usually 'localhost'
+PORT=$2 # usually '5672', accesses the tomcat web interface of rabbitmq for the kieker logs
+
+# TODO: implement here
