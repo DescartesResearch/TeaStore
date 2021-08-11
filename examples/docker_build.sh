c70e613b1df99f34589f64255b4a052844614ce6
simontrapp <simon.trapp115@gmail.com>
2021-08-11 11:28:49 +0200

    removed old unnecessary VERSION build arg

--- examples/docker_build.sh
+++ examples/docker_build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 push_flag='false'
 registry='descartesresearch'
-version='latest'
 
 print_usage() {
-  printf "Usage: docker_build.sh [-p] [-r REGISTRY_NAME] [-v VERSION]\n"
+  printf "Usage: docker_build.sh [-p] [-r REGISTRY_NAME]\n"
 }
 
-while getopts 'pr:v:' flag; do
+while getopts 'pr:' flag; do
   case "${flag}" in
     p) push_flag='true' ;;
     r) registry="${OPTARG}" ;;
-    v) version="${OPTARG}" ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -20,11 +18,11 @@ done
 docker build --no-cache=true -t "${registry}/teastore-base" ../utilities/tools.descartes.teastore.dockerbase/
 perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'/|g' ../services/tools.descartes.teastore.*/Dockerfile
 docker build -t "${registry}/teastore-registry" ../services/tools.descartes.teastore.registry/
-docker build --build-arg VERSION=${version} -t "${registry}/teastore-persistence" ../services/tools.descartes.teastore.persistence/
-docker build --build-arg VERSION=${version} -t "${registry}/teastore-image" ../services/tools.descartes.teastore.image/
-docker build --build-arg VERSION=${version} -t "${registry}/teastore-webui" ../services/tools.descartes.teastore.webui/
-docker build --build-arg VERSION=${version} -t "${registry}/teastore-auth" ../services/tools.descartes.teastore.auth/
-docker build --build-arg VERSION=${version} -t "${registry}/teastore-recommender" ../services/tools.descartes.teastore.recommender/
+docker build -t "${registry}/teastore-persistence" ../services/tools.descartes.teastore.persistence/
+docker build -t "${registry}/teastore-image" ../services/tools.descartes.teastore.image/
+docker build -t "${registry}/teastore-webui" ../services/tools.descartes.teastore.webui/
+docker build -t "${registry}/teastore-auth" ../services/tools.descartes.teastore.auth/
+docker build -t "${registry}/teastore-recommender" ../services/tools.descartes.teastore.recommender/
 perl -i -pe's|.*FROM '"${registry}"'/|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
 
 if [ "$push_flag" = 'true' ]; then
