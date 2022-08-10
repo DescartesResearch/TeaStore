43b3f5073c60439d2f9c99cfb977640cfcdbc681
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2022-08-10 15:41:36 +0200

    Update start.sh

--- utilities/tools.descartes.teastore.dockerbase/start.sh
+++ utilities/tools.descartes.teastore.dockerbase/start.sh
@@ -43,6 +43,6 @@ fi
 
 touch /usr/local/tomcat/bin/setenv.sh
 chmod +x /usr/local/tomcat/bin/setenv.sh
-echo 'export JAVA_OPTS="-javaagent:/kieker/agent/agent.jar -Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties -Daj.weaving.verbose=false -Dorg.aspectj.weaver.loadtime.configuration=aop.xml -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Daj.weaving.loadersToSkip=java.net.URLClassLoader -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"' > /usr/local/tomcat/bin/setenv.sh
+echo 'export JAVA_OPTS="-Dkieker.monitoring.configuration=/kieker/config/kieker.monitoring.properties -Daj.weaving.verbose=false -Dorg.aspectj.weaver.loadtime.configuration=aop.xml -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Daj.weaving.loadersToSkip=java.net.URLClassLoader -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"' > /usr/local/tomcat/bin/setenv.sh
 
 export CLASSPATH=$CLASSPATH:/usr/local/slf4j-simple-1.7.21.jar
